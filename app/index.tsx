613c59f4d16d797aba022e607aa11000025b8558
Aniket Majety <aniketmaj21@gmail.com>
2025-01-29 20:31:44 -0500

    first

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    
+      <View className='flex-1 items-center justify-center'>
+        <Text className='text-yellow-400'>Hello World!</Text>
+        <Link href="/profile">Go to profile</Link>
+      </View>
+    
+  );
+}
+
