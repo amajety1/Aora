a4d4fa9a4a6a485ac1252ee78ad827d421af1806
aniket majety <aniketmaj21@gmail.com>
2025-01-31 11:26:07 -0500

    changed from tsx to jsx. connected db

---
+++ app/index.jsx
@@ -0,0 +1,53 @@
+import { View, Text, StyleSheet, Image, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Redirect, router } from 'expo-router';
+import { ScrollView } from 'react-native';
+import CustomButton from '@/components/CustomButton';
+
+import { images} from '../constants';
+
+export default function Index() {
+  return (
+    
+      <SafeAreaView className='bg-primary h-full'>
+        <ScrollView contentContainerStyle={{height:'100%'}} className='bg-primary'>
+          <View className='w-full items-center min-h-[85vh]  px-4 pt-8'>
+            <Image 
+              source={images.logo}
+              className='w-[130px] h-[84px]' 
+              resizeMode="contain"
+            />
+
+            <Image 
+              source={images.cards}
+              className='max-w-[380px] w-full h-[300px]' 
+              resizeMode="contain"
+            />
+
+            <View className='relative mt-5'>
+              <Text className='font-bold text-center text-white text-3xl'>Discover endless possibilities with{' '}
+                <Text className='text-secondary-200'>Aora</Text>
+              </Text>
+
+              <Image 
+                source={images.path}
+                className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+                resizeMode="contain"
+              />
+            </View>
+
+            <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>Where creativity meets innovation: embark on your creative journey with Aora</Text>
+           <CustomButton 
+             title="Continue with Email"
+             handlePress={() => router.push("/sign-in")}
+             containerStyles="w-full mt-7"
+             textStyles="text-white text-base font-semibold"
+             isLoading={false} 
+           />
+          </View>
+        </ScrollView>
+        <StatusBar backgroundColor="#161622" barStyle="light-content" />
+      </SafeAreaView>
+    
+  );
+}
