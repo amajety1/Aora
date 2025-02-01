ba495039f96c17f17e8b081b8d558c81b4247f3d
Aniket Majety <aniketmaj21@gmail.com>
2025-02-01 11:24:51 -0500

    added video fetching

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { default as CustomButton } from './CustomButton';
+export { default as EmptyState } from './EmptyState';
+export { default as FormField } from './FormField';
+export { default as SearchInput } from './SearchInput';
+export { default as Trending } from './Trending';
+export { default as VideoCard } from './VideoCard';
